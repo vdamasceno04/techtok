ec36fd93b5728951aef0a4ac669680dda2d57faf
iLukSbr <lucasyfm@hotmail.com>
2023-09-12 13:00:12 -0300

    db export

---
+++ db/20230912_125655.sql
@@ -0,0 +1,210 @@
+CREATE DATABASE  IF NOT EXISTS `company` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `company`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: company
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `caixa de som`
+--
+
+DROP TABLE IF EXISTS `caixa de som`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `caixa de som` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `bateria` int unsigned DEFAULT NULL,
+  `conexao` varchar(45) NOT NULL,
+  `potencia` int unsigned NOT NULL,
+  `protecao` varchar(45) NOT NULL,
+  `led` varchar(45) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `caixa de som`
+--
+
+LOCK TABLES `caixa de som` WRITE;
+/*!40000 ALTER TABLE `caixa de som` DISABLE KEYS */;
+/*!40000 ALTER TABLE `caixa de som` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fone`
+--
+
+DROP TABLE IF EXISTS `fone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fone` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `conexao` varchar(45) NOT NULL,
+  `bateria` int unsigned DEFAULT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `audio` varchar(45) NOT NULL,
+  `led` varchar(45) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fone`
+--
+
+LOCK TABLES `fone` WRITE;
+/*!40000 ALTER TABLE `fone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fone` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mouse`
+--
+
+DROP TABLE IF EXISTS `mouse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mouse` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `botoes` int unsigned NOT NULL,
+  `conexao` varchar(45) NOT NULL,
+  `bateria` int unsigned DEFAULT NULL,
+  `dpi` int unsigned NOT NULL,
+  `led` varchar(45) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mouse`
+--
+
+LOCK TABLES `mouse` WRITE;
+/*!40000 ALTER TABLE `mouse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mouse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pen drive`
+--
+
+DROP TABLE IF EXISTS `pen drive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pen drive` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `capacidade` int unsigned NOT NULL,
+  `conexao` varchar(45) NOT NULL,
+  `leitura` int unsigned NOT NULL,
+  `escrita` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pen drive`
+--
+
+LOCK TABLES `pen drive` WRITE;
+/*!40000 ALTER TABLE `pen drive` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pen drive` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teclado`
+--
+
+DROP TABLE IF EXISTS `teclado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teclado` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `conexao` varchar(45) NOT NULL,
+  `bateria` int unsigned DEFAULT NULL,
+  `alcance` int unsigned DEFAULT NULL,
+  `layout` varchar(45) NOT NULL,
+  `switch` varchar(45) NOT NULL,
+  `led` varchar(45) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teclado`
+--
+
+LOCK TABLES `teclado` WRITE;
+/*!40000 ALTER TABLE `teclado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teclado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-12 12:59:34
