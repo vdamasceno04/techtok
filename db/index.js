73096d1fd59f11e75129d3bbe58dde4bba34841e
iLukSbr <lucasyfm@hotmail.com>
2023-09-13 14:38:08 -0300

    missing await correction

--- db/index.js
+++ db/index.js
@@ -16,11 +16,11 @@
         estoque:10,
         imagem:'./imgs/teclado.png'
     })
-    console.log(idTeclado)
+    // console.log(idTeclado)
     estoque = await db.getCell({table:'teclados',column:'estoque',id:1})
     // console.log(estoque)
     teclado = await db.getRow({table:'teclados',id:1})
     // console.log(teclado)
     teclados = await db.getTable('teclados')
-    // console.log(teclados)
+    console.log(teclados)
 })()
