43a98ba699a6f025652892dd643aa822765fb2ca
victor <vdamasceno04@gmail.com>
2023-10-08 18:14:38 -0300

    category.html is able to fetch respective table from api endpoint

--- category.js
+++ category.js
@@ -2,6 +2,7 @@ function getCategory(url){
     param = new URLSearchParams(url)
     cat = param.get('cat') //should match db table name
     console.log(cat)
+    return cat
 }
 
 async function printTable(url){
@@ -11,3 +12,19 @@ async function printTable(url){
     //const table = db.getTable(tablename)
     await console.log(db.getTable(tablename))
 }
+
+async function getProductsfromCategory(category){
+    const endpoint = 'http://localhost:3000/product/'+ category
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(data => {
+        const conteudoDiv = document.getElementById('dados');
+
+        // Defina o texto do parágrafo com os dados obtidos da API
+        conteudoDiv.textContent = JSON.stringify(data);
+        console.log(JSON.stringify(data))
+    })
+    .catch(error => console.log(error))
+}
+
+getProductsfromCategory(getCategory(window.location.search))
