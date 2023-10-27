fbbb769e0f15daa7d9d9870c92bd95fac7104966
victor <vdamasceno04@gmail.com>
2023-10-27 16:40:24 -0300

    need to make id auto increment in MYSQL

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const db = require("../../db/db.js")
+const db = require("../../db/db.js");
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/users', async (req, res) => {
@@ -19,15 +20,14 @@ router.get('/users:username', async (req, res) => {
     } catch(error){res.status(500).json({error: "falha ao acessar db"})}
   });
 
-router.post('/register', async (req, res) =>{
-    console.log("arrived register")
+router.post('/register', bodyParser.json(), async (req, res) =>{
     try{
-        info = {id: 4, login: 'rafa', password: 'rafa123', name: 'rafael',
-        email: 'rafael@gmail.com', superuser: 0x01};
-        req.body = JSON.stringify(info);
-        const newUser = info;
+        //info = {id: 4, login: 'rafa', password: 'rafa123', name: 'rafael',
+        //email: 'rafael@gmail.com', superuser: 0x01};
+        //req.body = JSON.stringify(info);
+        //const newUser = info;
         console.log('reqbody: ' + req.body)
-        await db.insertRow('users', newUser);
+        await db.insertRow('users', req.body);
         res.json(info)
     } catch(error){res.status(500).json({error: "falha ao acessar db"})}
 });
--- register.js
+++ register.js
@@ -19,7 +19,8 @@ function getEmail(){ //get email from html's filled box
 }
 
 async function sendRegisterData(usname, pass, uname, em){
-    const info = {username: usname, password: pass, name: uname, email: em}; 
+    const info = {login: usname, password: pass, name: uname, email: em}; 
+    console.log(info)
     await axios.post('http://localhost:3000/user/register', info)
     .then((response) => {
       console.log(response);
