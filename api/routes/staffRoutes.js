e3789cfd28b759bbea190d27366daefdcd3b4571
victor <vdamasceno04@gmail.com>
2023-11-17 16:41:21 -0300

    fix staff post route

--- api/routes/staffRoutes.js
+++ api/routes/staffRoutes.js
@@ -3,9 +3,11 @@ const db = require("../../db/db.js");
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-router.post('/mouse', bodyParser.json(), async (req, res) =>{
+router.post('/insert', bodyParser.json(), async (req, res) =>{
     try{
-       // await db.insertRow('', req.body);
+       await db.insertRow(req.body);
         res.json(res.status(200));
     } catch(error){res.status(500).json({error: "falha ao acessar db"})}
 });
+
+module.exports = router;
