3051347d259f9cb5b66257def998995f522a61c6
iLukSbr <lucasyfm@hotmail.com>
2023-11-28 21:03:02 -0300

    remember login

--- site/productInfo.js
+++ site/productInfo.js
@@ -49,16 +49,33 @@ function setModel(info){// set html product model field
     document.getElementById("model").innerHTML = 'Model: ' + info.model
 }
 
-function setProduct(info){// run all functions above
-    setTitle(info)
-    setName(info)
-    setImgPath(info)
-    setBrand(info)
-    setModel(info)
-    setPrice(info)
-    setStock(info)
-    setDescription(info)
-    setWarranty(info)
-}
+function setProduct(info) {
+    // Set the title
+    document.querySelector("title").textContent = getName(info);
+  
+    // Set the product name
+    document.getElementById("productName").innerHTML = getName(info);
+  
+    // Set the product image
+    document.getElementById("productImg").src = window.config.API_ENDPOINT + 'product/' + info.image_path;
+    document.getElementById("productImg").alt = getName(info);
+  
+    // Create an array of keys for the info object
+    var keys = Object.keys(info);
+  
+    // Loop through the keys
+    for (var i = 0; i < keys.length; i++) {
+      // Skip the 'image_path' key because it's used for the image source
+      if (keys[i] !== 'image_path') {
+        // Get the element by id
+        var element = document.getElementById(keys[i]);
+  
+        // If the element exists, set its innerHTML
+        if (element) {
+          element.innerHTML = keys[i].charAt(0).toUpperCase() + keys[i].slice(1) + ': ' + info[keys[i]];
+        }
+      }
+    }
+  }
 
 //TODO: SHOW SPECIFIC ATTRIBUTES FROM EACH CATEGORY
