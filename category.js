984cef22b94442db362155535fbea2ba461b3ec1
vdamasceno04 <vdamasceno04@gmail.com>
2024-01-26 19:29:28 -0300

    category.html has logo and background but still dont show product img

--- category.js
+++ category.js
@@ -5,6 +5,20 @@ function getCategory(url){
     return cat
 }
 
+function setImg(elementId, imagePath) {
+  const imgElement = document.getElementById(elementId);
+
+  if (imgElement) {
+    // Set the src attribute of the image element
+    imgElement.src = imagePath;
+
+    // Optional: Set alt attribute for accessibility
+    imgElement.alt = elementId;
+  } else {
+    console.error(`Element with ID '${elementId}' not found.`);
+  }
+}
+
 //fetches the whole category table as a json (TO BE OPTIMIZED)
 //need make buttons for each fetched product
 async function getProductsfromCategory(category){
