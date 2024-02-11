0a128d20f9bc255bfdfcf455b2fae1635eef0cac
vdamasceno04 <vdamasceno04@gmail.com>
2024-02-11 15:34:06 -0300

    endpoint ports config

---
+++ website/config.js
@@ -0,0 +1,8 @@
+window.config = {
+    API_PROTOCOL: 'https',
+    API_HOST: 'localhost',
+    API_PORT: 443
+}
+
+window.config.API_ENDPOINT = window.config.API_PROTOCOL + '://' + window.config.API_HOST + ':' + window.config.API_PORT + '/'
+  
